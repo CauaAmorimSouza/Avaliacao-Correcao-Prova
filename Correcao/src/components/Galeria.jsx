ba6ca65c219dee5c42a39777408c1bafa9d75c78
Cauã Souza <caua_a_souza@estudante.sesisenai.org.br>
2024-04-29 12:03:22 -0300

    Updates

--- Correcao/src/components/Galeria.jsx
+++ Correcao/src/components/Galeria.jsx
@@ -1,6 +1,6 @@
 function Galeria(){
     return(
-        <div className="galeria">
+        <div className="Galeria">
             <img src="Galeria 1.jfif" alt="" />
             <img src="Galeria 2.jfif" alt="" />
             <img src="Galeria 3.jfif" alt="" />
